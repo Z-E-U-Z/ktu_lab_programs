38f7513f405321a565b275c72f18ab511e7aaafe
Amal Nath M <amalnathm7@gmail.com>
2022-06-21 01:07:48 +0530

    HTTP Proxy server implemented

--- cnlab/expt8/server.c
+++ cnlab/expt8/server.c
@@ -3,20 +3,42 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
 
 char* get() {
-	return "Hello, Welcome!";
+	return "GET SUCCESS\n";
 }
 
 char* post(char* body) {
-	return "Body posted!";
+	char* temp = strdup(body);
+	sprintf(body, "POST %s SUCCESS\n", temp);
+	return body;
 }
 
-void main(int PORT) {
+char* put(char* body) {
+	char* temp = strdup(body);
+	sprintf(body, "PUT %s SUCCESS\n", temp);
+	return body;
+}
+
+char* patch(char* body) {
+	char* temp = strdup(body);
+	sprintf(body, "PATCH %s SUCCESS\n", temp);
+	return body;
+}
+
+void main(int argc, char *argv[]) {
+	int PORT;
+	if(argc == 2)
+		PORT = atoi(argv[1]);
+	else {
+		printf("Enter Web Server port!\n");
+		exit(1);
+	}
+	
 	int server_fd, client_fd;
 	struct sockaddr_in address;
 	int addrlen = sizeof(address);
-	char buff[20];
 	
 	if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("Socket creation failed!\n");
@@ -32,7 +54,11 @@ void main(int PORT) {
 		exit(1);
 	}
 	
+	char* buff;
+	
 	while(1) {
+		buff = malloc(100 * sizeof(char));
+		
 		if(listen(server_fd, 5) < 0) {
 			printf("Listening failed!\n");
 			exit(1);
@@ -43,35 +69,62 @@ void main(int PORT) {
 		if((client_fd = accept(server_fd, (struct sockaddr*) &address, (socklen_t*) &addrlen)) < 0) {
 			printf("Accept failed!\n");
 			exit(1);
-		} else {
-			printf("Request accepted.\n");
 		}
 		
-		if(recv(client_fd, buff, 20 * sizeof(char), 0) < 0) {
-			printf("Receive failed!\n");
+		if(recv(client_fd, buff, 6 * sizeof(char), 0) < 0) {
+			printf("Reeceive failed!\n");
 			exit(1);
 		} else {
 			printf("Request: %s\n", buff);
 			
-			if(!strcmp(buff, "get") {
+			if(!strcmp(buff, "get")) {
+				
 				buff = get();
 				
-				if(send(client_fd, buff, 20 * sizeof(char), 0) < 0)
-					printf("Receive failed!\n");
-			} else if(!strcmp(buff, "post") {
-				if(recv(client_fd, buff, 20 * sizeof(char), 0) < 0)
+				if(send(client_fd, buff, 100 * sizeof(char), 0) < 0)
+					printf("Send failed!\n");
+					
+			} else if(!strcmp(buff, "post")) {
+				
+				if(recv(client_fd, buff, 100 * sizeof(char), 0) < 0)
 					printf("Receive failed!\n");
 					
 				buff = post(buff);
-			} else {
-				buff = "Invalid request";
 				
-				if(send(client_fd, buff, 20 * sizeof(char), 0) < 0)
+				if(send(client_fd, buff, 100 * sizeof(char), 0) < 0)
+					printf("Send failed!\n");
+					
+			} else if(!strcmp(buff, "put")) {
+				
+				if(recv(client_fd, buff, 100 * sizeof(char), 0) < 0)
 					printf("Receive failed!\n");
+					
+				buff = put(buff);
+				
+				if(send(client_fd, buff, 100 * sizeof(char), 0) < 0)
+					printf("Send failed!\n");
+					
+			} else if(!strcmp(buff, "patch")) {
+				
+				if(recv(client_fd, buff, 100 * sizeof(char), 0) < 0)
+					printf("Receive failed!\n");
+					
+				buff = patch(buff);
+				
+				if(send(client_fd, buff, 100 * sizeof(char), 0) < 0)
+					printf("Send failed!\n");
+					
+			} else {
+				
+				buff = "INVALID REQUEST\n";
+				
+				if(send(client_fd, buff, 100 * sizeof(char), 0) < 0)
+					printf("Send failed!\n");
 			}
 		}
+		
+		close(client_fd);
 	}
 	
 	close(server_fd);
-	close(client_fd);
 }
