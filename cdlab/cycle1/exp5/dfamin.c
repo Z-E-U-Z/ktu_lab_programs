281e920b6cbffba29a4aa9a70c6b24bdd93f5df9
Amal Nath M <amalnathm7@gmail.com>
2022-11-23 15:33:00 +0530

    Minimisation of DFA

--- cdlab/cycle1/exp5/dfamin.c
+++ cdlab/cycle1/exp5/dfamin.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct trans {
 	char state1[3];
@@ -17,6 +18,8 @@ typedef struct trans1 {
 	char input;
 	state state2;
 } trans1;
+
+int store[10];
 
 // DFA data
 int n, f, m, l;
@@ -39,7 +42,7 @@ int isFinal(char state[]) {
 	return flag;
 }
 
-int goTo(char st[],  char c) {
+int goTo(char st[], char c) {
 	for(int i = 0; i < m; i++) {
 		if(!strcmp(table[i].state1, st) && table[i].input == c) {
 			for(int j = 0; j < n; j++) {
@@ -50,7 +53,110 @@ int goTo(char st[],  char c) {
 	}
 }
 
-void addState(state* states, int i, char st[]) {
+int contains(state st[], int n, char state[3]) {
+	int flag = 0;
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < st[i].len; j++) {
+			if(!strcmp(st[i].states[j], state))
+				flag = 1;
+		}
+	}
+	
+	return flag;
+}
+
+int cmpState(state state1, state state2) {
+	if(state1.len != state2.len)
+		return 0;
+	
+	int count = 0;
+	
+	for(int i = 0; i < state1.len; i++) {
+		for(int j = 0; j < state2.len; j++) {
+			if(!strcmp(state1.states[i], state2.states[j]))
+				count++;
+		}
+	}
+	
+	if(count == state1.len)
+		return 1;
+	else
+		return 0;
+}
+
+int containsState(state states[], int n, state st) {
+	int flag = 0;
+		
+	for(int i = 0; i < n; i++) {
+		if(cmpState(states[i], st))
+			flag = 1;
+	}
+	
+	return flag;
+}
+
+bool is_clique(int n, int graph[n][n], int b) {
+	for(int i = 1; i < b; i++) {
+		for(int j = i + 1; j < b; j++)
+			if(graph[store[i]][store[j]] == 1)
+				return false;
+	}
+	return true;
+}
+
+void combine(int n)
+{
+	states1[n1].len = 0;
+	
+	int flag = 0;
+	
+	for(int i = 1; i < n; i++) {
+		if(!contains(states1, n1, states[store[i]])) {
+			strcpy(states1[n1].states[states1[n1].len++], states[store[i]]);
+			flag = 1;
+		}
+	}
+	
+	if(flag)
+		n1++;
+}
+
+void findCliques(int n, int graph[n][n], int i, int l, int k) {
+	for (int j = i; j < n - (k - l); j++) {
+		store[l] = j;
+		
+		if (is_clique(n, graph, l + 1)) {
+			if (l < k)
+				findCliques(n, graph, j + 1, l + 1, k);
+			else
+				combine(l + 1);
+		}
+	}
+}
+
+void cpyState(state* state1, state state2) {
+	state1->len = state2.len;
+	
+	for(int i = 0; i < state2.len; i++) {
+		strcpy(state1->states[i], state2.states[i]);
+	}
+}
+
+void insertTrans(trans1* table, state state1, char input, state state2, int i) {
+	table[i].state1.len = state1.len;
+	
+	for(int j = 0; j < state1.len; j++) {
+		strcpy(table[i].state1.states[j], state1.states[j]);
+	}
+
+	table[i].state2.len = state2.len;
+	
+	for(int j = 0; j < state2.len; j++) {
+		strcpy(table[i].state2.states[j], state2.states[j]);
+	}
+	
+	table[i].input = input;
 }
 
 void main() {
@@ -95,10 +201,12 @@ void main() {
 	}
 	
 	// Minimising using Myhill-Nerode Theorem
+	printf("\nMinimised DFA\n");
+	
 	int mTbl[n][n];
 	
 	for(int i = 0; i < n; i++) {
-		for(int j = i + 1; j < n; j++) {
+		for(int j = 0; j < n; j++) {
 			mTbl[i][j] = 0;
 		}
 	}
@@ -108,6 +216,7 @@ void main() {
 		for(int j = i + 1; j < n; j++) {
 			if((isFinal(states[i]) && !isFinal(states[j])) || (!isFinal(states[i]) && isFinal(states[j])) ) {
 				mTbl[i][j] = 1;
+				mTbl[j][i] = 1;
 			}
 		}
 	}
@@ -127,8 +236,9 @@ void main() {
 					s1 = goTo(states[i], alpha[k]);
 					s2 = goTo(states[j], alpha[k]);
 					
-					if(mTbl[s1][s2]) {
+					if(mTbl[s1][s2] || mTbl[s2][s1]) {
 						mTbl[i][j] = 1;
+						mTbl[j][i] = 1;
 						flag = 1;
 					}
 				}
@@ -136,36 +246,98 @@ void main() {
 		}
 	} while(flag);
 	
-	// Combining states
-	n1 = 0;
 	for(int i = 0; i < n; i++) {
-		for(int j = i + 1; j < n; j++) {
-			if(!mTbl[i][j]) {
-				strcpy(states1[n1].states[0], states[i]);
-				strcpy(states1[n1].states[1], states[j]);
-				states1[n1].len = 2;
-				n1++;
-			}
-		}
-	}
-
-	for(int i = 0; i < n1; i++) {
-		for(int j = 0; j < states1[i].len; j++) {
-			printf("%s ", states1[i].states[j]);
-		}
-		printf("\n");
-	}
-	
-	// Transition table of min DFA
-	
-	// Start state of min DFA
-	
-	// Final states of min DFA
-	
-	/*for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
 			printf("%d ", mTbl[i][j]);
 		}
 		printf("\n");
-	}*/
+	}
+	
+	// Combining states
+	n1 = 0;
+	
+	for(int k = n; k > 0; k--) {
+		findCliques(n, mTbl, 0, 1, k);
+	}
+	
+	printf("\nStates:\n");
+	for(int i = 0; i < n1; i++) {
+		printf("{");
+		for(int j = 0; j < states1[i].len; j++) {
+			printf(" %s ", states1[i].states[j]);
+		}
+		printf("}\n");
+	}
+	
+	// Transition table of min DFA
+	m1 = 0;
+	
+	for(int i = 0; i < n1; i++) {
+		for(int j = 0; j < l; j++) {
+			for(int k = 0; k < m; k++) {
+				if(!strcmp(table[k].state1, states1[i].states[0]) && table[k].input == alpha[j]) {
+					for(int i1 = 0; i1 < n1; i1++) {
+						for(int j1 = 0; j1 < states1[i1].len; j1++) {
+							if(!strcmp(states1[i1].states[j1], table[k].state2)) {
+								insertTrans(table1, states1[i], alpha[j], states1[i1], m1++);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	printf("\nTransition table:\n");
+	for(int i = 0; i < m1; i++) {
+		printf("{");
+		for(int j = 0; j < table1[i].state1.len; j++) {
+			printf(" %s ", table1[i].state1.states[j]);
+		}
+		printf("}");
+
+		printf(" %c ", table1[i].input);		
+
+		printf("{");
+		for(int j = 0; j < table1[i].state2.len; j++) {
+			printf(" %s ", table1[i].state2.states[j]);
+		}
+		printf("}\n");
+	}
+	
+	// Start state of min DFA
+	for(int i = 0; i < n1; i++) {
+		for(int j = 0; j < states1[i].len; j++) {
+			if(!strcmp(states1[i].states[j], start)) {
+				cpyState(&start1, states1[i]);
+			}
+		}
+	}
+	
+	printf("\nStart state: {");
+	for(int i = 0; i < start1.len; i++) {
+		printf(" %s ", start1.states[i]);
+	}
+	printf("}\n");
+	
+	// Final states of min DFA
+	f1 = 0;
+	for(int i = 0; i < n1; i++) {
+		for(int j = 0; j < states1[i].len; j++) {
+			for(int k = 0; k < f; k++) {
+				if(!strcmp(states1[i].states[j], final[k]) && !containsState(final1, f1, states1[i])) {
+					cpyState(&final1[f1++], states1[i]);
+				}
+			}
+		}
+	}
+	
+	printf("\nFinal states:\n");
+	for(int i = 0; i < f1; i++) {
+		printf("{");
+		for(int j = 0; j < final1[i].len; j++) {
+			printf(" %s ", final1[i].states[j]);
+		}
+		printf("}\n");
+	}
 }
